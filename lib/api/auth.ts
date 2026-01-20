9bffd7c4e0959173200de6a2347631fb21df0d3d
Heroicspirit <astarana21@gmail.com>
2026-01-20 14:49:34 +0545

    added auth.ts, acios,ts and endpoints.ts

---
+++ lib/api/auth.ts
@@ -0,0 +1,37 @@
+import axios from "./axios";
+import { LoginValue, RegisterData } from "@/app/(auth)/schema";
+import {API} from "./endpoints";
+
+
+export const register = async (registerData: RegisterData)=>{
+    try {
+        const response = await axios.post(
+            API.AUTH.REGISTER, 
+            registerData
+        );
+        return response.data; 
+    }catch (err: Error | any){
+        throw new Error (
+            err.response?.data?.message
+            || err.message
+            || 'Registration Failed'
+        );
+    }
+}
+
+
+export const login = async (loginData: LoginValue)=>{
+    try {
+        const response = await axios.post(
+            API.AUTH.LOGIN, 
+            loginData
+        );
+        return response.data; 
+    }catch (err: Error | any){
+        throw new Error (
+            err.response?.data?.message
+            || err.message
+            || 'Login Failed'
+        );
+    }
+}
